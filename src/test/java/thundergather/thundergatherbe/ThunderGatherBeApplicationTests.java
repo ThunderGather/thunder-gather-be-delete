89801bda4a00d120e7d948d9d389fc7623eef4d9
Namgyu11 <hagaji3@naver.com>
2024-07-30 00:42:00 +0900

    Initial commit

---
+++ src/test/java/thundergather/thundergatherbe/ThunderGatherBeApplicationTests.java
@@ -0,0 +1,13 @@
+package thundergather.thundergatherbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThunderGatherBeApplicationTests {
+
+      @Test
+      void contextLoads() {
+      }
+
+}
