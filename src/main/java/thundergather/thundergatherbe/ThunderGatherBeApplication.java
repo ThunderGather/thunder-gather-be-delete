500f046c10b8d25eef13ce90c454c13b27999d5d
Namgyu11 <hagaji3@naver.com>
2024-07-30 00:42:00 +0900

    Initial commit

---
+++ src/main/java/thundergather/thundergatherbe/ThunderGatherBeApplication.java
@@ -0,0 +1,13 @@
+package thundergather.thundergatherbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThunderGatherBeApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(ThunderGatherBeApplication.class, args);
+      }
+
+}
