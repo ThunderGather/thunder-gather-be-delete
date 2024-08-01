3dd47c7b7ce913f9084017ab230aca0f3eb1d283
Namgyu11 <hagaji3@naver.com>
2024-08-01 23:54:44 +0900

    add: 프로젝트 초기 설정 및 엔티티 추가
    - AWS S3, Redis, JpaAuditing 등의 설정 파일 추가
    - Member, Post, MeetingMember 엔티티 생성
    - GlobalException 및 관련 에러 처리 클래스 추가
    - 빌드 스크립트에 AWS S3 관련 의존성 추가

---
+++ src/main/java/thundergather/thundergatherbe/member/entity/Member.java
@@ -0,0 +1,69 @@
+package thundergather.thundergatherbe.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import thundergather.thundergatherbe.global.entity.BaseEntity;
+import thundergather.thundergatherbe.meeting.entity.MeetingMember;
+import thundergather.thundergatherbe.post.entity.Post;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member extends BaseEntity {
+
+      @Id
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+      private Long id;
+
+      @Column(nullable = false, unique = true)
+      private String email;
+
+      @Column(nullable = false)
+      private String password;
+
+      @Column(nullable = false)
+      private String nickname;
+
+      private String profileImageUrl;
+
+      @Builder.Default
+      @OneToMany(mappedBy = "member")
+      private List<Post> posts = new ArrayList<>();
+
+      @Builder.Default
+      @OneToMany(mappedBy = "member")
+      private List<MeetingMember> myMeetingList = new ArrayList<>();
+
+      public void addPost(Post post) {
+            this.posts.add(post);
+            post.assignMember(this);
+      }
+
+      public void removePost(Post post) {
+            this.posts.remove(post);
+            post.assignMember(null);
+      }
+
+      public void addMeetingMemberList(MeetingMember meetingMember) {
+            this.myMeetingList.add(meetingMember);
+            meetingMember.assignToMember(this);
+      }
+
+      public void removeMeetingMemberList(MeetingMember meetingMember) {
+            this.myMeetingList.remove(meetingMember);
+            meetingMember.assignToMember(null);
+      }
+
+}
